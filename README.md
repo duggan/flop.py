b9b48f4e04fb58255ab7167a0d490f0ec6895618
Ross Duggan <rduggan@engineyard.com>
2012-04-17 00:54:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# flop.py
+Musical floppy drives in Python.
+
+## Tests
+Performs tests on the floppy drive array.
+Each drive has a different timbre and range, it's important to test these in order to ensure the array
+stays in tip top shape.
---
+++ tests/test-drives.py
@@ -0,0 +1,57 @@
+from midiutil.MidiFile import MIDIFile
+
+''' Test the range of the drives and some other stuff '''
+
+TestDrives = MIDIFile(3)
+
+pitchmap = {
+        0: {
+            'lower': 24,
+            'upper': 66
+            },
+        1: {
+            'lower': 24,
+            'upper':67,
+            },
+        2: {
+            'lower': 24,
+            'upper': 60
+            }
+}
+
+time = 0
+duration = 10
+volume = 100 #  Makes no difference to floppy drive
+
+print "Trying the lower pitch boundary..."
+for drive, pitch in pitchmap.iteritems():
+    TestDrives.addTrackName(drive, time, "Position %d" % drive)
+    channel = drive
+
+    TestDrives.addNote(drive, channel, pitch['lower'], time, duration, volume)
+    time += duration
+
+print "Trying the upper pitch boundary..."
+for drive, pitch in pitchmap.iteritems():
+    TestDrives.addTrackName(drive, time, "Position %d" % drive)
+    channel = drive
+
+    TestDrives.addNote(drive, channel, pitch['upper'], time, duration, volume)
+    time += duration
+
+# Try the scale!
+duration = 1
+
+for drive, pitch in pitchmap.iteritems():
+    print "Trying %d MIDI pitches for drive %d" % (len(range(pitch['lower'],
+        pitch['upper'])), drive)
+    for n in range(pitch['lower'], pitch['upper']):
+        TestDrives.addTrackName(drive, time, "Position %d" % drive)
+        channel = drive
+
+        TestDrives.addNote(drive, channel, n, time, duration, volume)
+        time += duration
+
+with open("testdrives.mid", "wb") as midi:
+    TestDrives.writeFile(midi)
+
